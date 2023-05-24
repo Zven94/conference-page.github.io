5b04a249716f95881e1a9afc551a4948f5d21474
Zven94 <125831572+Zven94@users.noreply.github.com>
2023-05-24 07:19:07 -0300

    add style to burgerBtn

--- main.js
+++ main.js
@@ -5,15 +5,15 @@
 // define my const
 
 const burgerBtn = document.querySelector('.bugerBtn');
+const burgerMenu = document.querySelector('.burger-menu');
 
 // define function for button element
 function burgerMenuDisplay() {
   console.log('you click me');
 //get the state of the burger menu (display:block/none)
-  const burgerMenu = document.querySelector('.burger-menu');
   const menuDisplay = window.getComputedStyle(burgerMenu).display;
 // if the state is none I change it to block  
-  if (menuDisplay === 'none' ){
+  if (menuDisplay === 'none'){
     burgerMenu.style.display = 'block'
     burgerBtn.style.display = 'none'
   } else {
@@ -37,6 +37,21 @@ const exitBtn = document.querySelector('.exitButton');
 // event to  burger button
 
 exitBtn.addEventListener('click', burgerMenuDisplay);
+
+
+//-----------------------------------------------------
+//  menu buttons close the menu when cliked
+//-----------------------------------------------------
+
+// define my const
+
+const homeBtn = document.querySelector('.burger-menu-home-page');
+const aboutBtn = document.querySelector('.burger-menu-aboutPage-button');
+
+// event to  burger button
+
+homeBtn.addEventListener('click', burgerMenuDisplay);
+aboutBtn.addEventListener('click', burgerMenuDisplay);
 
 //-----------------------------------------------------
 //-----------------------------------------------------
